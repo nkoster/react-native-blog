ea232b4f989653acbaf4808cf5db16424dc950b0
Niels Koster <n.koster@portavita.eu>
2020-05-05 23:12:20 +0200

    Initial, IndexScreen starter

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const IndexScreen = _ => {
+    return (
+        <View>
+            <Text>Index Screen</Text>
+        </View>
+    )
+}
+
+export default IndexScreen
