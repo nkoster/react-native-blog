db977665848110eddc53b52b5ecb2166248d1767
Niels Koster <n.koster@portavita.eu>
2020-05-06 00:25:41 +0200

    Render drone posts in a FlatList

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,21 @@
 import React, { useContext } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
 import DroneContext from '../context/DroneContext'
 
 const IndexScreen = _ => {
-    const value = useContext(DroneContext)
+    const dronePosts = useContext(DroneContext)
     return (
         <View>
             <Text>Index Screen</Text>
-            <Text>{value.name}</Text>
+            <FlatList
+                data={dronePosts}
+                keyExtractor={dronePost => dronePost.title}
+                renderItem={({item}) => {
+                    return (
+                        <Text>{item.title}</Text>
+                    )
+                }}
+            />
         </View>
     )
 }
