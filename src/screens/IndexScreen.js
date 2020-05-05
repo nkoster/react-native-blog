fc9b1be30651b0a92bb163d385dbc0d140ea99f2
Niels Koster <n.koster@portavita.eu>
2020-05-06 00:10:47 +0200

    Show provided context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, View } from 'react-native'
+import DroneContext from '../context/DroneContext'
 
 const IndexScreen = _ => {
+    const value = useContext(DroneContext)
     return (
         <View>
             <Text>Index Screen</Text>
+            <Text>{value.name}</Text>
         </View>
     )
 }
