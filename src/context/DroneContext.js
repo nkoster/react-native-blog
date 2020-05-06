f9251949f71312e3745c98f7b9792ead2923010f
Niels Koster <n.koster@portavita.eu>
2020-05-06 22:06:35 +0200

    Replace useState for useReducer

--- src/context/DroneContext.js
+++ src/context/DroneContext.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 
 const DroneContext = React.createContext()
 
+const droneReducer = (state, action) => {
+    switch(action.type) {
+        case 'add_dronepost':
+            return [...state, { title: `drone post ${state.length + 1}`}]
+        default:
+            return state
+    }
+}
 export const DroneProvider = ({children}) => {
-    const [dronePosts, setDronePosts] = useState([])
+    const [dronePosts, dispatch] = useReducer(droneReducer, [])
     const addDronePost = _ => {
-        setDronePosts([...dronePosts, {
-            title: `drone post ${dronePosts.length + 1}`
-        }])
+        dispatch({ type: 'add_dronepost'})
     }
     return (
-        <DroneContext.Provider value={{
-            data: dronePosts,
-            addDronePost
-        }}>
+        <DroneContext.Provider value={{ data: dronePosts, addDronePost }}>
             {children}
         </DroneContext.Provider>
     )
