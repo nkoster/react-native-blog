ca6d5483a958c4981a521ae10332ee3f7df177c9
Niels Koster <n.koster@portavita.eu>
2020-05-09 00:08:36 +0200

    ShowScreen

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const ShowScreen = _ => {
+    return (
+        <View>
+            <Text>Show Screen</Text>
+        </View>
+    )
+}
+
+export default ShowScreen
