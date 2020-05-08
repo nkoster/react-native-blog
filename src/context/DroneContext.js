0be4c736a039163581f63dd09671cb9c25b1ce3f
Niels Koster <n.koster@portavita.eu>
2020-05-08 22:52:28 +0200

    Delete a post

--- src/context/DroneContext.js
+++ src/context/DroneContext.js
@@ -3,7 +3,12 @@ import createDataContext from './createDataContext'
 const droneReducer = (state, action) => {
     switch(action.type) {
         case 'add_dronepost':
-            return [...state, { title: `drone post ${state.length + 1}`}]
+            return [...state, {
+                id: Math.floor(Math.random() * 99999),
+                title: `drone post ${state.length + 1}`
+            }]
+        case 'del_dronepost':
+            return state.filter(dronePost => dronePost.id !== action.payload)
         default:
             return state
     }
@@ -13,8 +18,12 @@ const addDronePost = dispatch => {
     return _ => dispatch({ type: 'add_dronepost'})
 }
 
+const delDronePost = dispatch => {
+    return id => dispatch({ type: 'del_dronepost', payload: id })
+}
+
 export const { Context, Provider } = createDataContext(
     droneReducer,
-    { addDronePost },
+    { addDronePost, delDronePost },
     []
 )
