d45f8dd2e88297f5f08939c06f22201a9efa709e
Niels Koster <n.koster@portavita.eu>
2020-05-08 22:52:50 +0200

    Delete a post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,25 +1,28 @@
 import React, { useContext } from 'react'
-import { Text, View, FlatList, Button } from 'react-native'
+import { Text, View, FlatList, Button, TouchableOpacity } from 'react-native'
 import { Context as DroneContext } from '../context/DroneContext'
 import { Ionicons } from '@expo/vector-icons'
 
 const IndexScreen = _ => {
-    const { state, addDronePost } = useContext(DroneContext)
+    const { state, addDronePost, delDronePost } = useContext(DroneContext)
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button
                 title='add Drone post'
                 onPress={addDronePost}
             />
             <FlatList
                 data={state}
-                keyExtractor={dronePost => dronePost.title}
+                keyExtractor={dronePost => String(dronePost.id)}
                 renderItem={({item}) => {
                     return (
                         <View style={styles.row}>
-                            <Text style={styles.title}>{item.title}</Text>
-                            <Ionicons name="ios-beer" size={24} color="brown" />
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity
+                                onPress={_ => delDronePost(item.id)}
+                            >
+                                <Ionicons name="ios-trash" size={28} color='#666' />
+                            </TouchableOpacity>
                         </View>
                     )
                 }}
@@ -32,7 +35,7 @@ const styles = {
     row: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        padding: 5,
+        padding: 8,
         borderBottomWidth: 1,
         borderColor: 'silver'
     },
