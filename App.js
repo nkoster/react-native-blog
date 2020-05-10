cf6059b7802311030ddc51d779929c79dc67861c
Niels Koster <n.koster@portavita.eu>
2020-05-11 00:16:24 +0200

    add EditScreen

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { createStackNavigator } from 'react-navigation-stack'
 import IndexScreen from './src/screens/IndexScreen'
 import ShowScreen from './src/screens/ShowScreen'
 import CreateScreen from './src/screens/CreateScreen'
+import EditScreen from './src/screens/EditScreen'
 
 import { Provider as DroneProvider } from './src/context/DroneContext'
 
 const navigator = createStackNavigator({
   IndexScreen,
   ShowScreen,
-  CreateScreen
+  CreateScreen,
+  EditScreen
 }, {
   initialRouteName: 'IndexScreen',
   defaultNavigationOptions: {
