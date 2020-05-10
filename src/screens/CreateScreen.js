b61dddcb6696e377045378fa3c742a309b57eb2f
Niels Koster <n.koster@portavita.eu>
2020-05-10 21:33:41 +0200

    Local state for form, plus, process form, update Context

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,37 @@
-import React, { useContext } from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { Text, View, TextInput, Button } from 'react-native'
 import { Context } from '../context/DroneContext'
 
 const CreateScreen = ({navigation}) => {
-    const { state } = useContext(Context)
+    // app state:
+    const { addDronePost } = useContext(Context)
+    // local state, for the form:
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
     return (
         <View>
-            <Text>Create Screen</Text>
+            <Text style={styles.label} >Title</Text>
+            <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)} />
+            <Text style={styles.label}>Content</Text>
+            <TextInput style={styles.input} value={content} onChangeText={text => setContent(text)} />
+            <Button title='Add Drone Post' onPress={_ => addDronePost(title, content)} />
         </View>
     )
 }
 
+const styles = {
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'silver',
+        borderRadius: 4,
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    label: {
+        fontSize: 20,
+        margin: 6
+    }
+}
 export default CreateScreen
