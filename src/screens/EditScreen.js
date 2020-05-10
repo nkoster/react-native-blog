7cbb62beb3c02381b5d632e763afd916ed32f3c7
Niels Koster <n.koster@portavita.eu>
2020-05-11 00:16:02 +0200

    Initial, EditScreen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const EditScreen = _ => {
+    return (
+        <View>
+            <Text>Edit Screen</Text>
+        </View>
+    )
+}
+
+export default EditScreen
