06b23799390fb8bb2c48aa48d438f71bd138a728
Niels Koster <n.koster@portavita.eu>
2020-05-10 20:48:56 +0200

    Add CreateScreen

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import IndexScreen from './src/screens/IndexScreen'
 import ShowScreen from './src/screens/ShowScreen'
+import CreateScreen from './src/screens/CreateScreen'
+
 import { Provider as DroneProvider } from './src/context/DroneContext'
 
 const navigator = createStackNavigator({
   IndexScreen,
-  ShowScreen
+  ShowScreen,
+  CreateScreen
 }, {
   initialRouteName: 'IndexScreen',
   defaultNavigationOptions: {
