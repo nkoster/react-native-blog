9ce6c138cc567201067ace6a8b2152845a385425
Niels Koster <n.koster@portavita.eu>
2020-05-12 20:47:59 +0200

    Get post data from Context and use useState for local state

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState, useContext } from 'react'
+import { Text, View, TextInput } from 'react-native'
+import { Context } from '../context/DroneContext'
 
 const EditScreen = ({navigation}) => {
-    const dronePost = navigation.getParam('id')
+    const { state } = useContext(Context)
+    const dronePost = state.find(p => p.id === navigation.getParam('id'))
+    const [ title, setTitle ] = useState(dronePost.title)
+    const [ content, setContent] = useState(dronePost.content)
     return (
         <View>
-            <Text>Edit Screen {dronePost}</Text>
+            <Text>Edit Title ({title})</Text>
+            <TextInput
+                value={title}
+                onChangeText={setTitle}
+            />
+            <Text>Edit Content</Text>
+            <TextInput
+                value={content}
+                onChangeText={setContent}
+            />
         </View>
     )
 }
